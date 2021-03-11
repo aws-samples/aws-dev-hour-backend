7dbe2f1589f84056c7f39f2add8ca2beb63d384c
Ben Newton <bjnewton@amazon.com>
2021-03-11 12:36:41 +1100

    Updated buildspec

--- lib/awsdevhour-backend-pipeline-stack.ts
+++ lib/awsdevhour-backend-pipeline-stack.ts
@@ -50,9 +50,8 @@ export class AwsdevhourBackendPipelineStack extends Stack {
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
-        buildCommand: 'rm ./reklayer/pillow-goes-here.txt && wget https://awsdevhour.s3-accelerate.amazonaws.com/pillow.zip && unzip pillow.zip && mv ./python ./reklayer && rm pillow.zip',
-        // buildCommand: 'npm run build',
-        // synthCommand: 'npm run cdk synth'
+        buildCommand: 'rm ./reklayer/pillow-goes-here.txt && wget https://awsdevhour.s3-accelerate.amazonaws.com/pillow.zip && unzip pillow.zip && mv ./python ./reklayer && rm pillow.zip && npm run build',
+        synthCommand: 'npm run cdk synth'
       })
     });
     
